3f3e692883bc65a16a12c1b69ea7bf5e043d0c56
Ashley Williams <awilliams1026@student.ccc.edu>
2020-11-10 20:03:44 -0600

    Initial Commit

---
+++ HotelManzana/AddRegistrationTableViewController.swift
@@ -0,0 +1,183 @@
+//
+//  AddRegistrationTableViewController.swift
+//  HotelManzana
+//
+//  Created by MXC Swift on 11/10/20.
+//
+
+import UIKit
+
+class AddRegistrationTableViewController: UITableViewController {
+    
+    let checkInDatePickerCellIndexPath = IndexPath(row:1, section: 1)
+    let checkOutDatePickerCellIndexPath =
+        IndexPath(row:3, section: 1)
+    
+    var isCheckInDatePickerShown: Bool = false {
+        didSet {
+            checkInDatePicker.isHidden = !isCheckInDatePickerShown
+        }
+    }
+    var isCheckOutDatePickerShown: Bool = false {
+        didSet {
+            checkOutDatePicker.isHidden = !isCheckOutDatePickerShown
+        }
+    }
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
+    
+    
+    @IBOutlet weak var checkInDateLabel: UILabel!
+    
+    @IBOutlet weak var checkInDatePicker: UIDatePicker!
+    
+    
+    
+    @IBOutlet weak var checkOutDateLabel: UILabel!
+    
+    
+    @IBOutlet weak var checkOutDatePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let midnightToday = Calendar.current.startOfDay(for: Date())
+        checkInDatePicker.minimumDate = midnightToday
+        checkInDatePicker.date = midnightToday
+        
+        
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    func updateViews () {
+        checkOutDatePicker.minimumDate = checkInDatePicker.date.addingTimeInterval(86400)
+       
+        let dateFormatter = DateFormatter ()
+        dateFormatter.dateStyle = .medium
+        checkInDateLabel.text = dateFormatter.string(from: checkInDatePicker.date)
+        checkOutDateLabel.text = dateFormatter.string(from: checkOutDatePicker.date)
+    }
+
+    @IBAction func doneBarButtonTapped(_ sender: UIBarButtonItem) {
+        
+        let firstName = firstNameTextField.text ?? ""
+        let lastName = lastNameTextField.text ?? ""
+        let email = emailTextField.text ?? ""
+        let checkInDate = checkInDatePicker.date
+        let checkOutDate = checkOutDatePicker.date
+        
+        print("DONE TAPPED")
+        print("firstName: \(firstName)")
+        print("lastName: \(lastName)")
+        print("email: \(email)")
+        print("checkIn: \(checkInDate)")
+        print("checkOut: \(checkOutDate)")
+    }
+    
+    
+    @IBAction func datePickerValueChanged(_sender: UIDatePicker){
+        updateViews()
+    }
+    
+    // MARK: - Table view data source
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 2
+//    }
+
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 0
+//    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case checkInDatePickerCellIndexPath:
+           
+            if isCheckInDatePickerShown {
+                return 216.0
+            } else {
+                return 0.0
+        }
+        case checkOutDatePickerCellIndexPath:
+            if isCheckOutDatePickerShown {
+                return 216.0
+            } else {
+                return 0.0
+    }
+        default:
+            return 44.0
+        }
+    }
+    
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+       
+        }
